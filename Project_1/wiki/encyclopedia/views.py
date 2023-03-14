548065db638f1e8c1635bd28fe6a01e5e647202f
Zach Palmer <zach@Zachs-MBP.lan>
2023-03-14 13:04:31 -0500

    Fixed unused functions

--- Project_1/wiki/encyclopedia/views.py
+++ Project_1/wiki/encyclopedia/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.files.base import ContentFile
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.core.files.storage import default_storage
 from django.urls import reverse
 from django.contrib import messages
