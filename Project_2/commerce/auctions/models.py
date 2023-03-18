8f817176fd7a50aeb8c6276304640d98905e767a
Zach Palmer <zach@Zachs-MBP.lan>
2023-03-18 02:33:48 -0500

    Created initial model designs

--- Project_2/commerce/auctions/models.py
+++ Project_2/commerce/auctions/models.py
@@ -1,32 +1,37 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.db import models
 
 
 class User(AbstractUser):
-    id = models.IntegerField(primary_key=True)
-    cash = models.PositiveIntegerField(default=1000)
+    id = models.AutoField(primary_key=True)
+    cash = models.DecimalField(max_digits=8, decimal_places=2)
+    account_name = models.CharField(max_length=20, default='N/A')
+    account_email = models.EmailField(max_length=64, default='N/A')
 
 
 class Listing(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='listing')
     time_left = models.DurationField()
-    posting = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
     image = models.ImageField()
 
 
 class Bid(models.Model):
-    amount = models.CharField(max_length=64)
-    id = models.IntegerField(primary_key=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='bid')
     listing_id = models.ForeignKey(
         Listing, on_delete=models.CASCADE, related_name='bid')
+    posted = models.DateTimeField(default=timezone.now)
 
 
 class Comment(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comment')
     content = models.TextField(max_length=500)
+    posting = models.DateTimeField(default=timezone.now)
