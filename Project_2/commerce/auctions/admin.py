6ca73a5fed21b32249b39c709822f015924aae6d
Zach Palmer <zach@Zachs-MBP.lan>
2023-05-17 22:22:03 -0500

    Final commit

--- Project_2/commerce/auctions/admin.py
+++ Project_2/commerce/auctions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import User, Bid, Listing, Comment
+from .models import User, Bid, Listing, Comment, WatchlistItem
 
 
 class ListingAdmin(admin.ModelAdmin):
@@ -9,8 +9,21 @@ class ListingAdmin(admin.ModelAdmin):
                     'posting', 'price')
 
 
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'listing')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'listing')
+
+
+class WatchlistItemAdmin(admin.ModelAdmin):
+    list_dsplay = ('user', 'listing')
+
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Bid)
+admin.site.register(Bid, BidAdmin)
 admin.site.register(Listing, ListingAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(WatchlistItem, WatchlistItemAdmin)
