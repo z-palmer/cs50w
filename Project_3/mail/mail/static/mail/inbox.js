d77930a59f8324cc405d3f702c0a3aa63a16bb97
Zach Palmer <zach@Zachs-MBP.lan>
2023-06-01 09:01:53 -0500

    Second commit

--- Project_3/mail/mail/static/mail/inbox.js
+++ Project_3/mail/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -16,6 +16,28 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  document.querySelector('#compose-form').onsubmit = function () {
+
+    // variablize form values to insert into table
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    // fetch to insert to API
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        load_mailbox('sent');
+      });
+  }
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -23,11 +45,49 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if (mailbox === 'inbox') {
+
+    // load inbox
+    fetch('/emails/inbox')
+      .then(response => response.json())
+      .then(emails => {
+        console.log(emails);
+        emails.forEach(element => {
+          const email = document.createElement('div');
+          email.className = 'email-preview';
+          email.innerHTML = `<h5><strong>From:</strong> ${element.sender} 
+          <strong>Subject:</strong> ${element.subject} 
+          <strong>Time:</strong> ${element.timestamp}<h5>`;
+          document.querySelector('#emails-view').append(email);
+        });
+      });
+  }
+
+  else if (mailbox === 'sent') {
+
+    // load sent mail
+    fetch('/emails/sent')
+      .then(response => response.json())
+      .then(emails => {
+        console.log(emails);
+      });
+  }
+
+  else if (mailbox === 'archive') {
+
+    // load archive
+    fetch('/emails/archive')
+      .then(response => response.json())
+      .then(emails => {
+        console.log(emails);
+      });
+  }
 }
